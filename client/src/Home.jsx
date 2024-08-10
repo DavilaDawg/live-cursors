d8c0613026e6f9f2e7ad2749065d67beb1f1b146
Isabella <idavila2021@gmail.com>
2024-08-10 16:26:13 +0100

    fix: colors working and only see other cursors

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -3,30 +3,27 @@ import useWebSocket from "react-use-websocket";
 import throttle from "lodash.throttle";
 import { Cursor } from "./components/Cursor";
 
-const renderCursors = (users, color, currentId) => {
-  return Object.keys(users)
-    .filter((uuid) => uuid !== currentId)
-    .map((uuid) => {
-      const user = users[uuid];
-
-      return (
-        <Cursor key={uuid} point={[user.state.x, user.state.y]} color={color} />
-      );
-    });
+const renderCursors = (users, color) => {
+  return Object.keys(users).map((uuid) => {
+    const user = users[uuid];
+    return (
+      <Cursor key={uuid} point={[user.state.x, user.state.y]} color={color} />
+    );
+  });
 };
 
 const renderUsersList = (users) => {
   return (
     <ul>
       {Object.keys(users).map((uuid) => {
-        return <li key={uuid}>{JSON.stringify(users[uuid])}</li>;
+        return <li key={uuid}>{users[uuid].username}</li>;
       })}
     </ul>
   );
 };
 
 export const Home = ({ username, color }) => {
-  const [currentId, setCurrentId] = useState(null);
+  const [otherUsers, setOtherUsers] = useState({});
 
   const WS_URL = "ws://localhost:8000";
   const { sendJsonMessage, lastJsonMessage } = useWebSocket(WS_URL, {
@@ -40,7 +37,7 @@ export const Home = ({ username, color }) => {
   useEffect(() => {
     sendJsonMessage({
       x: 0,
-      y: 0
+      y: 0,
     });
     window.addEventListener("mousemove", (e) => {
       sendJsonMessageThrottled.current({
@@ -51,20 +48,23 @@ export const Home = ({ username, color }) => {
   }, []);
 
   useEffect(() => {
-    if (lastJsonMessage && !currentId) {
-      setCurrentId(lastJsonMessage.currentId);
+    if (lastJsonMessage) {
+      const users = lastJsonMessage;
+      const filteredUsers = Object.keys(users).reduce((acc, uuid) => {
+        if (users[uuid].username !== username) {
+          acc[uuid] = users[uuid];
+        }
+        return acc;
+      }, {});
+      setOtherUsers(filteredUsers);
     }
-  }, [lastJsonMessage, currentId]);
+  }, [lastJsonMessage, username]);
 
-
-  if (lastJsonMessage) {
-    return (
-      <>
-        {renderCursors(lastJsonMessage, color)}
-        {renderUsersList(lastJsonMessage)}
-      </>
-    );
-  }
-
-  return <h1> Hello, {username} </h1>;
+  return (
+    <>
+      {renderCursors(otherUsers, color)}
+      {renderUsersList(otherUsers)}
+      <h1>Hello, {username}</h1>
+    </>
+  );
 };
--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 const http = require("http");
 const { WebSocketServer } = require("ws");
 const uuidv4 = require("uuid").v4;
-
 const url = require("url");
 
 const server = http.createServer();
@@ -11,57 +10,51 @@ const port = 8000;
 const connections = {};
 const users = {};
 
-const broadcast = () => { // every time server recieves message broadcast list of users to everyone, shows whos online and their state 
-    Object.keys(connections).forEach(uuid => {
-        const connection = connections[uuid]
-        const message = JSON.stringify(users)
-        connection.send(message)
-    })
-}
+const broadcast = () => {
+  // every time server recieves message broadcast list of users to everyone, shows whos online and their state
+  Object.keys(connections).forEach((uuid) => {
+    const connection = connections[uuid];
+    const message = JSON.stringify(users);
+    connection.send(message);
+  });
+};
 
-const handleMessage = (bytes, uuid) => { // message = {"x": 0 , "y":100} (message is the state) or user.state.x = message.x
-    const message = JSON.parse(bytes.toString())
+const handleMessage = (bytes, uuid) => {
+  // message = {"x": 0 , "y":100} (message is the state) or user.state.x = message.x
+  const message = JSON.parse(bytes.toString());
+  const user = users[uuid];
+  user.state = message;
+  // if(messageType = currorUpdate) ...
+  broadcast(); //everytime state updates (message received), update user state and broadcast
+};
 
-    const user = users[uuid]
-    user.state = message
-    // if(messageType = currorUpdate) ... 
+const handleClose = (uuid) => {
+  console.log(`${users[uuid].username} disconnected`);
+  delete connections[uuid];
+  delete users[uuid];
+  broadcast();
+};
 
-    broadcast() //everytime state updates (message received), update user state and broadcast 
-
-    //console.log(`${user.username} updated their state: ${JSON.stringify(user.state)}`)
-}
-
-const handleClose = uuid => {
-    console.log(`${users[uuid].username} disconnected`)
-
-    delete connections[uuid]
-    delete users[uuid]
-
-    broadcast()
-}
-
-wsServer.on("connection", (connection, request) => {
-  // ws://localhost:8000?username=Alex
-
+wsServer.on("connection", (connection, request) => { // ws://localhost:8000?username=Alex
   const { username } = url.parse(request.url, true).query;
   const uuid = uuidv4();
   console.log(`${username} connected`);
 
   connections[uuid] = connection; // key is uuid in collections obj
 
-  users[uuid]= { 
+  users[uuid] = {
     username,
-    state: { // everytime user moves curser send and update to server with websocket with new positons and periodiclly send a broadcast to all connected users with list of users and positons 
-        // x: 0,
-        // y: 0,
-        // typing: true
-        // onlineStatus: "On best space ever"
-    }
-  }
+    state: {
+      // everytime user moves curser send and update to server with websocket with new positons and periodiclly send a broadcast to all connected users with list of users and positons
+      // x: 0,
+      // y: 0,
+      // typing: true
+      // onlineStatus: "On best space ever"
+    },
+  };
 
-  connection.on("message", message => handleMessage(message, uuid))
-  connection.on("close",  () => handleClose(uuid)) 
-
+  connection.on("message", (message) => handleMessage(message, uuid));
+  connection.on("close", () => handleClose(uuid));
 });
 
 server.listen(port, () => {
